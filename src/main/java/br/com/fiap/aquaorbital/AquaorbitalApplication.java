49a15387d3dcaa991d82aaecb453949e34f37f7e
Pedro Brum <pbrum191@gmail.com>
2026-05-27 17:28:21 -0300

    feat: initial Spring Boot project structure

---
+++ src/main/java/br/com/fiap/aquaorbital/AquaorbitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.aquaorbital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquaorbitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquaorbitalApplication.class, args);
+	}
+
+}
