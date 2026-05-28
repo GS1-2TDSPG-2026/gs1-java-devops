f3364e9cb5ff4b4a55c34e73be6a162eabc11ad9
PedroBrum-DEV <pbrum191@gmail.com>
2026-05-28 19:00:07 -0300

    feat(api):finalizando Repository

--- src/main/java/br/com/fiap/aquaorbital/repository/CreditoCarbonoRepository.java
+++ src/main/java/br/com/fiap/aquaorbital/repository/CreditoCarbonoRepository.java
@@ -1,4 +1,15 @@
 package br.com.fiap.aquaorbital.repository;
 
-public class CreditoCarbonoRepository {
+import br.com.fiap.aquaorbital.entity.CreditoCarbono;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CreditoCarbonoRepository extends JpaRepository<CreditoCarbono, Long> {
+    List<CreditoCarbono> findByFazendaId(Long fazendaId);
+    List<CreditoCarbono> findByStatus(String status);
+    Optional<CreditoCarbono> findByHashAuditoria(String hashAuditoria);
+    List<CreditoCarbono> findByLoteId(Long loteId);
 }
